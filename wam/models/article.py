ac098a20ae1557e820ba235a7ca017679de9f310
floorvanos <138969944+floorvanos@users.noreply.github.com>
2023-10-13 16:49:50 +0200

    Update article.py

--- wam/models/article.py
+++ wam/models/article.py
@@ -1,13 +1,5 @@
 from datetime import datetime, time
 from odoo import fields, models
-STATE = [
-    ('concept', 'Concept'),
-    ('approved', 'Approved'),
-    ('awaits', 'Awaits Publishing'),
-    ('published', 'Published'),
-    ('unpublished', 'Unpublished'),
-    ('archived', 'Archived'),
-]
 
 class article(models.Model):
     _name = "article"
@@ -17,16 +9,9 @@ class article(models.Model):
     name = fields.Char('Article Name', required=True, translate=True, default="De titel")
     intro_text = fields.Html('Intro text', translate=True, default="De intro tekst")
     main_text = fields.Html('Main text', translate=True, default="De hoofd tekst" )
-    state = fields.Selection(STATE, string='Article Status', store=True,
-        help="It indicates the article status.\n" default='concept')
+    published = fields.Boolean('Published', default=True)
     publish_up = fields.Datetime('Publish Up')
     publish_down = fields.Datetime('Publish Down')
-    # select a portal user as author?
-    author = fields.Char('Author', required=True, translate=True)
-    # -----------------------------------
-    show_date = fields.Boolean('Show publishing date', default=False)
-    show_author = fields.Boolean('Show author', default=False)
-    
     
     active = fields.Boolean('Active', default=True)
     
