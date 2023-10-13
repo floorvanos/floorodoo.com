e6882a459b1d7dbde19350a17e20b5bf9f7d876b
floorvanos <138969944+floorvanos@users.noreply.github.com>
2023-10-13 09:03:36 +0200

    Add files via upload

--- article/__init__.py
+++ article/__init__.py
@@ -1,13 +1,1 @@
-{
-    'name': 'Website Article Manager',
-    'category': 'Website',
-    'sequence': 20,
-    'summary': 'Website Article Manager',
-    'author': 'Floor van Os',
-    'website': 'https://www.floorodoo.com/',
-    'version': '0.12',
-    'depends': [
-        'base',
-    ],
-    'application': True,
-}
+from . import models
