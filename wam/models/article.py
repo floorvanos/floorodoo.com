0ca577d7be748c49c39d5a8edbec1dbab3df0ac3
floorvanos <138969944+floorvanos@users.noreply.github.com>
2023-10-13 10:40:15 +0200

    Update article.py

--- wam/models/article.py
+++ wam/models/article.py
@@ -1,18 +1,12 @@
 from odoo import fields, models
 
-
 class article(models.Model):
     _name = "article"
     _description = "Article "
     _order = "sequence"
 
     name = fields.Char('Article Name', required=True, translate=True)
-    categ_id = fields.Many2one(
-        'article.category', 'Article Category',
-        change_default=True, default=_get_default_category_id, group_expand='_read_group_categ_id',
-        required=True)
+    categ_id = fields.Many2one('article.category', 'Article Category',change_default=True, default=_get_default_category_id, group_expand='_read_group_categ_id',required=True)
     intro_text = fields.Html('Intro text', translate=True)
     main_text = fields.Html('Main text', translate=True)
-    
-        
     active = fields.Boolean('Active', default=True)
