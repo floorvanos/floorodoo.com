5a9bba69b3852d649d276df51381596059b474f0
floorvanos <138969944+floorvanos@users.noreply.github.com>
2023-10-13 10:35:53 +0200

    Update article.py

--- wam/models/article.py
+++ wam/models/article.py
@@ -7,6 +7,12 @@ class article(models.Model):
     _order = "sequence"
 
     name = fields.Char('Article Name', required=True, translate=True)
-    active = fields.Boolean('Active', default=True)
-    sequence = fields.Integer('Sequence', default=10)
+    categ_id = fields.Many2one(
+        'article.category', 'Article Category',
+        change_default=True, default=_get_default_category_id, group_expand='_read_group_categ_id',
+        required=True)
+    intro_text = fields.Html('Intro text', translate=True)
+    main_text = fields.Html('Main text', translate=True)
     
+        
+    active = fields.Boolean('Active', default=True)
