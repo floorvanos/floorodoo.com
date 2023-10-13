7fb9bd563b816ad7f8c85f2666173414860de66a
floorvanos <138969944+floorvanos@users.noreply.github.com>
2023-10-13 10:11:44 +0200

    Add files via upload

---
+++ wam/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Website Article Manager',
+    'category': 'Website',
+    'sequence': 20,
+    'summary': 'Website Article Manager',
+    'author': 'Floor van Os',
+    'website': 'https://www.floorodoo.com/',
+    'version': '0.12',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/article_views.xml',
+        'views/wam_menus.xml',
+    ],
+    'application': True,
+}
