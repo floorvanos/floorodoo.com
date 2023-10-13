2e225f7f2281565d17cbddcdd06a7feb83877005
floorvanos <138969944+floorvanos@users.noreply.github.com>
2023-10-13 13:52:07 +0200

    Update article.py

--- wam/models/article.py
+++ wam/models/article.py
@@ -6,23 +6,7 @@ class article(models.Model):
     _description = "Article "
     _order = "id"
     
-    @tools.ormcache()
-    def _get_default_category_id(self):
-        # Deletion forbidden (at least through unlink)
-        return self.env.ref('article.article_category_all')
-
-    def _read_group_categ_id(self, categories, domain, order):
-        category_ids = self.env.context.get('default_categ_id')
-        if not category_ids and self.env.context.get('group_expand'):
-            category_ids = categories._search([], order=order, access_rights_uid=SUPERUSER_ID)
-        return categories.browse(category_ids)
-
     name = fields.Char('Article Name', required=True, translate=True)
-     categ_id = fields.Many2one(
-        'article.category', 'Article Category',
-        change_default=True, default=_get_default_category_id, group_expand='_read_group_categ_id',
-        required=True)
-
     intro_text = fields.Html('Intro text', translate=True)
     main_text = fields.Html('Main text', translate=True)
     published = fields.Boolean('Published', default=False)
@@ -30,4 +14,3 @@ class article(models.Model):
     publish_down = fields.Datetime('Publish Down')
     
     active = fields.Boolean('Active', default=True)
-    
