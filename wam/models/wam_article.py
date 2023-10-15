8b014781639f6838173c2e9c418d0807cd49234c
floorvanos <138969944+floorvanos@users.noreply.github.com>
2023-10-15 14:40:42 +0200

    Update wam_article.py

--- wam/models/wam_article.py
+++ wam/models/wam_article.py
@@ -12,14 +12,25 @@ class WamArticle(models.Model):
     main_text = fields.Html('Main text')
     publish_up = fields.Datetime('Publish Up')
     publish_down = fields.Datetime('Publish Down')
+    published = fields.Bool('Published', compute="_compute_is_published")
     author_id = fields.Many2one('res.users', string='Author', index=True, default=lambda self: self.env.user)
     category_id = fields.Many2one('wam.article.category', string='Category', index=True)
     tag_ids = fields.Many2many('wam.article.tag', string='Tags', index=True)
     
     active = fields.Boolean('Active', default=True)
     
+    @api.depends("publish_up", "publish_down")
+    def _compute_is_published(self):
+        now = datetime.now()
+        for record in self:
+            if record.publish_up and record.publish_down:
+                record.published = record.publish_up <= now < record.publish_down
+            elif record.publish_up and not record.publish_down:
+                record.published = record.publish_up <= now
+    
     @api.constrains('publish_down')
     def check_publish_down(self):
         for record in self:
             if record.publish_down and record.publish_down <= record.publish_up:
                 raise ValidationError("Publish down cannot be before publish up ")
+
