7bc64e9403c89c4d3d77d1495f0f3c4ed39727b6
floorvanos <138969944+floorvanos@users.noreply.github.com>
2023-10-15 14:28:25 +0200

    Update wam_article.py

--- wam/models/wam_article.py
+++ wam/models/wam_article.py
@@ -7,27 +7,17 @@ class WamArticle(models.Model):
     _description = "Article "
     _order = "id"
     
-    name = fields.Char('Article Name', required=True, translate=True)
-    intro_text = fields.Html('Intro text', translate=True)
-    main_text = fields.Html('Main text', translate=True)
-    published = fields.Boolean('Published', compute="_compute_is_published", store=True)
+    name = fields.Char('Article Name', required=True)
+    intro_text = fields.Html('Intro text')
+    main_text = fields.Html('Main text')
     publish_up = fields.Datetime('Publish Up')
     publish_down = fields.Datetime('Publish Down')
-    author_id = fields.Many2one('res.users', string='Author', index=True, tracking=True, default=lambda self: self.env.user)
-    category_id = fields.Many2one('wam.article.category', string='Category', index=True, tracking=True)
-    tag_ids = fields.Many2many('wam.article.tag', string='Tags', index=True, tracking=True)
+    author_id = fields.Many2one('res.users', string='Author', index=True, default=lambda self: self.env.user)
+    category_id = fields.Many2one('wam.article.category', string='Category', index=True)
+    tag_ids = fields.Many2many('wam.article.tag', string='Tags', index=True)
     
     active = fields.Boolean('Active', default=True)
     
-    @api.depends('publish_up', 'publish_down')
-    def _compute_is_published(self):
-        now = fields.Datetime.now()
-        for record in self:
-            if record.publish_up and record.publish_down:
-                record.published = record.publish_up <= now < record.publish_down
-            elif record.publish_up and not record.publish_down:
-                record.published = record.publish_up <= now
-                
     @api.constrains('publish_down')
     def check_publish_down(self):
         for record in self:
