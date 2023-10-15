b202031d02bb5074d2cde0e9cae135853a7a4a0a
floorvanos <138969944+floorvanos@users.noreply.github.com>
2023-10-15 09:05:28 +0200

    Update wam_article.py

--- wam/models/wam_article.py
+++ wam/models/wam_article.py
@@ -29,7 +29,5 @@ class WamArticle(models.Model):
 
     def _reverse_is_published(self):
         now = fields.Datetime.now()
-            for record in self:
-                if record.published:
-                    record.publish_up = now
+          
         
