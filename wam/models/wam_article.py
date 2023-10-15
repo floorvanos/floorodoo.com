962d39a147b3f447a85b1816877e1bf64523f4b2
floorvanos <138969944+floorvanos@users.noreply.github.com>
2023-10-15 13:02:41 +0200

    Update wam_article.py

--- wam/models/wam_article.py
+++ wam/models/wam_article.py
@@ -1,5 +1,6 @@
 from datetime import datetime, time
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 class WamArticle(models.Model):
     _name = "wam.article"
@@ -27,3 +28,8 @@ class WamArticle(models.Model):
             elif record.publish_up and not record.publish_down:
                 record.published = record.publish_up <= now
                 
+    @api.constrains('publish_down')
+    def check_publish_down(self):
+        for record in self:
+            if record.publish_down <= record.publish_up:
+                raise ValidationError("Publish down cannot be before publish up ")
