29729e36b2ff15d7804f54a33910ce47276b0eda
floorvanos <138969944+floorvanos@users.noreply.github.com>
2023-10-24 12:56:53 +0200

    Update wam_article_category.py

--- wam/models/wam_article_category.py
+++ wam/models/wam_article_category.py
@@ -10,4 +10,16 @@ class WamArticleCategory(models.Model):
     publish_up = fields.Datetime('Publish Up')
     publish_down = fields.Datetime('Publish Down')
     active = fields.Boolean('Active', default=True)
-    
+    article_ids = fields.One2many('wam.article.line', 'category_ids', string="Articles")
+
+class WamArticleLine(model.Model):
+
+    _name = "wam.article.line"
+    _description = "Wam Article"
+    _oder = "name"
+
+    name = fields.Char("Name", required=True, translate=True)
+    published = fields.Boolean('Published', store=True)
+    author_id = fields.Many2one('res.users', string='Author', index=True, default=lambda self: self.env.user)
+    category_ids = fields.Many2many('wam.article.category', string='Category', index=True)
+    tag_ids = fields.Many2many('wam.article.tag', string='Tags', index=True)
