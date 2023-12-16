fb69239542869cb49daa1bb6f69935729d82283a
floorvanos <info@floorvanos.nl>
2023-12-16 10:12:49 +0100

    manifest

---
+++ blogarticles/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Blog Articles Manager',
+    'version': '0.1.001',
+    'category': 'Website',
+    'summary': 'Manage all your blog articles',
+    'website': 'https://floorodoo.com',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'application': True
+}
