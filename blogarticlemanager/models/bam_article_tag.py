745e2d6ca5a18b81836987dcf24056257680e5a6
floorvanos <info@floorvanos.nl>
2023-12-21 15:18:09 +0100

    tags

--- blogarticlemanager/models/bam_article_tag.py
+++ blogarticlemanager/models/bam_article_tag.py
@@ -1,13 +1,41 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from random import randint
+
+from odoo import api, fields, models
 
 
-class ArticleTag(models.Model):
+class BamArticleTagCategory(models.Model):
+    _name = "bam.article.tag.category"
+    _description = "Article Tag Category"
+    _order = "sequence"
+
+    def _default_sequence(self):
+        """
+        Here we use a _default method instead of ordering on 'sequence, id' to
+        prevent adding a new related stored field in the 'bam.article.tag' model that
+        would hold the category id.
+        """
+        return (self.search([], order="sequence desc", limit=1).sequence or 0) + 1
+
+    name = fields.Char("Name", required=True, translate=True)
+    sequence = fields.Integer('Sequence', default=_default_sequence)
+    tag_ids = fields.One2many('bam.article.tag', 'category_id', string="Tags")
+
+
+class BamArticleTag(models.Model):
     _name = "bam.article.tag"
-    _description = "BAM Article Tag"
+    _description = "Article Tag"
+    _order = "category_sequence, sequence, id"
 
-    #article type fields
-    
-    name = fields.Char('Article Tag Name', required=True)
+    def _default_color(self):
+        return randint(1, 11)
+
+    name = fields.Char("Name", required=True, translate=True)
+    sequence = fields.Integer('Sequence', default=0)
+    category_id = fields.Many2one("bam.article.tag.category", string="Category", required=True, ondelete='cascade')
+    category_sequence = fields.Integer(related='category_id.sequence', string='Category Sequence', store=True)
+    color = fields.Integer(
+        string='Color Index', default=lambda self: self._default_color(),
+        help='Tag color. No color means no display in kanban or front-end, to distinguish internal tags from public categorization tags.')
