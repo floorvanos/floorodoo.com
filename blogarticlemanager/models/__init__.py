df0e8d930afb41543192a44390faa8b8eb850ab5
floorvanos <info@floorvanos.nl>
2023-12-24 09:54:25 +0100

    doc!

--- blogarticlemanager/models/__init__.py
+++ blogarticlemanager/models/__init__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 # from . import documents_mixin
-from . import document
+from . import documents_document
 from . import bam_article_type
 from . import bam_article_category
 from . import bam_article_tag
