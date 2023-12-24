a90693af3291fb94953d0ebf1880ce4c1eb57172
floorvanos <info@floorvanos.nl>
2023-12-24 09:49:39 +0100

    doc

--- blogarticlemanager/models/__init__.py
+++ blogarticlemanager/models/__init__.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 # from . import documents_mixin
+from . import document
 from . import bam_article_type
 from . import bam_article_category
 from . import bam_article_tag
--- blogarticlemanager/models/documents_mixin.py
+++
@@ -1,6 +0,0 @@
-# part of BAM
-
-from odoo import models
-
-class DocumentMixin(models.Model):
-    _inherit = "documents.mixin"
