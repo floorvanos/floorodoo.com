7169597be867da9547fb76309dc848b9d9000d97
floorvanos <info@floorvanos.nl>
2023-12-24 09:09:50 +0100

    Update __init__.py

--- blogarticlemanager/models/__init__.py
+++ blogarticlemanager/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import documents_mixin
+# from . import documents_mixin
 from . import bam_article_type
 from . import bam_article_category
 from . import bam_article_tag
