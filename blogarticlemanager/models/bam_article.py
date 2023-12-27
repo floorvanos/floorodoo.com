9b578561487000c0715b5ee87c301d982b811024
floorvanos <info@floorvanos.nl>
2023-12-27 17:15:41 +0100

    Update bam_article.py

--- blogarticlemanager/models/bam_article.py
+++ blogarticlemanager/models/bam_article.py
@@ -90,13 +90,13 @@ class Article(models.Model):
     @api.depends("publish_up","archive","trash")
     def _compute_state(self):
         for record in self:
-            if record.publish_up and record.trash and record.trash < fields.Datetime.now():
+            if record.publish_up and record.trash and record.trash <= fields.Datetime.now():
                 record.state = "trashed"
-            elif record.publish_up and record.archive and record.archive < fields.Datetime.now():
+            elif record.publish_up and record.archive and record.archive <= fields.Datetime.now():
                 record.state = "archived"
-            elif record.publish_up and record.publish_up < fields.Datetime.now():
+            elif record.publish_up and record.publish_up <= fields.Datetime.now():
                 record.state = "published"
-            elif record.publish_up and record.publish_up > fields.Datetime.now():
+            elif record.publish_up and record.publish_up => fields.Datetime.now():
                 record.state = "queued"
             else:
                 record.state = "draft"
@@ -111,8 +111,8 @@ class Article(models.Model):
     @api.constrains('publish_up', 'archive', 'trash', '')
     def _check_dates(self):
         for article in self:
-            if article.publish_up and article.archive and article.archive < article.publish_up:
+            if article.publish_up and article.archive and article.archive <= article.publish_up:
                 raise ValidationError(_('The archiving date cannot be earlier than the publishing date.'))
-            if article.publish_up and article.trash and article.trash < article.publish_up:
+            if article.publish_up and article.trash and article.trash <= article.publish_up:
                 raise ValidationError(_('The trashing date cannot be earlier than the publishing date.'))
                 
